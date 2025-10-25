e135645111d20bd3118ff7ea21730471c1d1da35
Tofirat Jahan Tisha <tofirattisha@gmail.com>
2025-10-25 11:46:05 +0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import route from"./routes/userRoute.js";
+import cors from "cors";
+
+dotenv.config();
+
+const app=express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const PORT=process.env.PORT||8000;
+const MONGOURL=process.env.MONGO_URL;
+
+
+mongoose
+.connect(MONGOURL)
+.then(()=>{
+
+    console.log("DB Connected Succesfully")
+    app.listen(PORT,()=>{
+        console.log(`server is running on port:${PORT}`)
+    });
+})
+.catch((error)=>console.log(error));
+
+app.use("/api",route);
